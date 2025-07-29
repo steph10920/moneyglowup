6965fa8ef5254928d506cb6bc8c961abde0ab2e8
Steph10920 <sashioyas@gmail.com>
2025-07-29 10:48:33 +0300

    solutions tab has been modified

--- src/components/Solutions.jsx
+++ src/components/Solutions.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../styles/Solutions.css";
 
 const Solutions = () => {
+  const [open, setOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const toggleDropdown = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div className="solutions">
-      <h1>Solutions</h1>
-      <ul>
-        <li id="setting-smart-goals">Setting SMART Financial Goals</li>
-        <li id="budgeting">Budgeting</li>
-        <li id="saving">Saving</li>
-        <li id="investing">Investing</li>
-        <li id="wealth-creation">Wealth Creation</li>
-      </ul>
+    <div className="dropdown-wrapper" ref={menuRef}>
+      <div className="dropdown-toggle" onClick={toggleDropdown}>
+        Solutions &#9662;
+      </div>
+      {open && (
+        <ul className="dropdown-menu">
+          <li><a href="#smart-goals">Setting SMART Financial Goals</a></li>
+          <li><a href="#budgeting">Budgeting</a></li>
+          <li><a href="#saving">Saving</a></li>
+          <li><a href="#investing">Investing</a></li>
+          <li><a href="#wealth-creation">Wealth Creation</a></li>
+        </ul>
+      )}
     </div>
   );
 };
